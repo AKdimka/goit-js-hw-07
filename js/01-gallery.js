9c874cbc2441e6bd778f7d8b0fb71fc2c3e2f20c
AKdimka <88268598+AKdimka@users.noreply.github.com>
2021-12-14 21:00:57 +0200

    hw-fix-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,41 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 
 const container = document.querySelector(".gallery");
-
-
 const markupGallery = createGalleryMarkup(galleryItems);
 
 container.insertAdjacentHTML("beforeend", markupGallery);
-
 container.addEventListener('click', onContainerClick)
-
 
 function createGalleryMarkup(gallery) {
 	return gallery
 		.map(({ preview, original, description }) => {
 			return `
-        <div class="gallery__item">
-            <a class="gallery__link" href="large-image.jpg" onclick="event.preventDefault()">
-                <img
-                class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}"
-                />
-            </a>
-        </div>`;
+				<div class="gallery__item">
+						<a class="gallery__link" href="large-image.jpg" onclick="event.preventDefault()">
+								<img
+								class="gallery__image"
+								src="${preview}"
+								data-source="${original}"
+								alt="${description}"
+								/>
+						</a>
+				</div>`;
 		})
 		.join("");
 }
-
 
 function onContainerClick(event) {
 	if (!event.target.classList.contains("gallery__image")) {
 		return;
 	}
 	const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}">
-    `
+		<img src="${event.target.dataset.source}">
+		`
 	)
 	instance.show()
 
@@ -45,9 +40,7 @@ function onContainerClick(event) {
 		if (isEscKey) {
 			instance.close();
 		}
-
 	}
 
 	window.addEventListener("keydown", onKeyboardEscAction);
-
-}    
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,32 +4,19 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const container = document.querySelector(".gallery");
-
-
 const markupGallery = createGalleryMarkup(galleryItems);
 
 container.insertAdjacentHTML("beforeend", markupGallery);
-
-container.addEventListener('click', onContainerClick)
-
 
 function createGalleryMarkup(gallery) {
 	return gallery
 		.map(({ preview, original, description }) => {
 			return `
-        <div class="gallery__item">
-            <a class="gallery__link" href="${original}" onclick="event.preventDefault()">
+            <a class="gallery__item" href="${original}">
                 <img class="gallery__image" src="${preview}" alt="${description}" />
-            </a>
-        </div>`;
+            </a>`;
 		})
 		.join("");
 }
 
-new SimpleLightbox('.gallery__item a', { captionsData: 'alt', captionDelay: '250' });
-
-function onContainerClick(event) {
-	if (!event.target.classList.contains("gallery__item")) {
-		return;
-	}
-}      
+new SimpleLightbox('.gallery__item', { captionsData: 'alt', captionDelay: '250' });
