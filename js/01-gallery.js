c0a11c3cf0f5dfd7d22b023f0393c450e88f747f
Dmytro Bryhadyr <88268598+AKdimka@users.noreply.github.com>
2023-07-02 10:37:03 +0300

    practice again 1.2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,20 +4,46 @@ const galleryContainer = document.querySelector('.gallery');
 
 galleryContainer.addEventListener('click', onGalleryElClick)
 
+
+
 function onGalleryElClick(e) {
 	if (e.target.nodeName !== 'IMG') {
 		return
 	}
-	console.log(e.target.nodeName)
+	window.addEventListener('keydown', onEscPress)
+
+	const lageImg = e.target.dataset.source
+
+	const instance = basicLightbox.create(`
+	<img src=${lageImg} width="800" height="600">
+`)
+
+	function onEscPress(e) {
+		const keyName = e.key;
+		console.log(keyName)
+		if (keyName !== 'Escape') {
+			return
+		}
+		instance.close()
+		window.removeEventListener(onEscPress)
+	}
+
+	instance.show()
 }
 
 function markupCreate(items) {
 	return items.map(({ preview, original, description }) => {
-		return `<li class='gallery__item'>
-			<a class='gallery__link' href=${original} onclick='event.preventDefault()'>
-				<img class='gallery__image' src=${preview} alt=${description} />
-			</a>
-		</li>`
+		return `
+		<li class="gallery__item">
+		<a class="gallery__link" href=${original} onclick='event.preventDefault()'>
+			<img
+				class="gallery__image"
+				src=${preview}
+				data-source=${original}
+				alt=${description}
+			/>
+		</a>
+	</li>`
 	}).join('')
 }
 
