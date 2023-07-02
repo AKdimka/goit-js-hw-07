e879f69d0fa3e5f006cb3dfa04df4fbe2b22e95e
Dmytro Bryhadyr <88268598+AKdimka@users.noreply.github.com>
2023-07-02 11:48:08 +0300

    practice again 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js'
+
+const gallery = document.querySelector('ul.gallery')
+
+gallery.innerHTML = markupGallery(galleryItems)
+
+function markupGallery(items) {
+	return items.map(({ preview, original, description }) => {
+		return `
+		<li class="gallery__item">
+		<a class="gallery__link" href=${original} onClick='event.preventDefault()'>
+			 <img class="gallery__image" src=${preview} alt=${description} />
+		</a>
+ </li>`
+	}).join('')
+}
+
+new SimpleLightbox('.gallery__item a');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
@@ -12,11 +60,11 @@ function createGalleryMarkup(gallery) {
 	return gallery
 		.map(({ preview, original, description }) => {
 			return `
-            <a class="gallery__item" href="${original}">
-                <img class="gallery__image" src="${preview}" alt="${description}" />
-            </a>`;
+						<a class="gallery__item" href="${original}">
+								<img class="gallery__image" src="${preview}" alt="${description}" />
+						</a>`;
 		})
 		.join("");
 }
 
-new SimpleLightbox('.gallery__item', { captionsData: 'alt', captionDelay: '250' });
+new SimpleLightbox('.gallery__item', { captionsData: 'alt', captionDelay: '250' }); */
